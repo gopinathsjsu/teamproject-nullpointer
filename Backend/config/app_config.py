16d32787ce2190e918d58f8d8cee3cbca91e436f
Rajath Nattoji Rajaram <41383649+rnrnattoji@users.noreply.github.com>
2023-09-20 17:19:56 -0700

    Develop (#1)
    * Initialized backend folder structure
    
    * Finished setting up Backend Flask Framework
    
    * Added .gitignore file in the Backend repo
    
    * Finished setting up MongoDB connections

---
+++ Backend/config/app_config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# server specific variables
+INSTANCE_NAME = "cmpe202"
+
+# mongo db config
+mongodb_username = "cmpe202"
+mongodb_password = "m5mg84zEtM0le7J9"
+db_name = "cmpe202_db"
