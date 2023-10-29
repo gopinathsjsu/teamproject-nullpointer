2b027e1cff8dccef449ec499732c164e81beb266
Dimitar Dimitrov <73325837+Jellyfish25@users.noreply.github.com>
2023-10-29 12:20:05 -0700

    Feature/login - API + Storage (#5)
    * initial login setup
    
    * formatted login page and enabled data retrieval
    
    * included hyperlinks, generalized css (using index.scss, and initialized the register page
    
    * updated register page functionaly *needs API still
    
    * merging with dev branch
    
    * Created Login/Register API && checked for valid input
    
    * added localStorage capabilities to login

--- Frontend/src/Views/Login/Login.jsx
+++ Frontend/src/Views/Login/Login.jsx
@@ -1,7 +1,12 @@
 import "./Login.scss";
 import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
+
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [error, setError] = useState('');
+
   //username storage
   const [username, setUsername]=useState(null);
   //password storage
@@ -9,9 +14,29 @@ const Login = () => {
 
   //on button press, call getLogin
   const getLogin = () => {
+    const user = {username, password};
+    setError();
+
+    fetch("http://localhost:8005/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify(user)
+    })
+    .then((resp) => {
+      if(resp.ok) {
+        localStorage.setItem('user', JSON.stringify(user));
+        console.log(localStorage.getItem('user'));
+        console.log('logged in successfully');
+        navigate("/");
+      }
+      else {
+        console.log(resp)
+        setError("Error: Username or password is incorrect");
+      }
+    })
     //return login info
     // implement backend to login (verify if account exists && if not, give error)
-    console.log("username: " + username + " password: " + password);
+    //console.log("username: " + username + " password: " + password);
   }
   
   function updateUsername(e) {
@@ -29,8 +54,8 @@ const Login = () => {
       <div className="header-container">
         <h1 className="background-container">
           <div className="loginPanel-container">
-            <p style={{ marginBottom: "30px" }}>Sign In</p>
-
+            
+            <p className="title-margin">Sign In</p>
             {/* username login field */}
             <div className="input-container">
                 <input 
@@ -43,7 +68,6 @@ const Login = () => {
             </div>
 
             <p style={{ marginBottom: "20px"}}></p>
-
             {/* password login field */}
             <div className="input-container">
                 <input 
@@ -57,17 +81,15 @@ const Login = () => {
             </div>
             
             <p style={{ marginBottom: "20px"}}></p>
-            <button onClick={getLogin}>
-              Sign In
-            </button>
-  
-            <p style={{marginTop: "84px"}}>
+            <button onClick={getLogin}> Sign In </button>
+            <p className="error-message">{error}</p>
 
+            <p style={{marginTop: "84px"}}></p>
               {/* link to access register page*/}
               <a href="/register" className="link">
                 Don't have an account? Register
               </a>
-            </p>
+            {/* </p> */}
           </div>
         </h1>
       </div>
