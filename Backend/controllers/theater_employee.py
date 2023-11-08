47e1914ab63b5eb9247cb532989af345bf8bcac5
Rajath N R <rajath.nr117@gmail.com>
2023-11-07 22:03:39 -0800

    Locations CRUD operation implemented

--- Backend/controllers/theater_employee.py
+++ Backend/controllers/theater_employee.py
@@ -59,3 +59,51 @@ def delete_movie(movie_id, *args, **kwargs):
     logger.info("Movie Deleted : ID ({0})".format(str(movie_id)))
 
     return jsonify({"message": "Movie Deletion Successfull"})
+
+
+@theater_employee.route('/api/theater_employee/insert_location', methods=['POST'])
+@check_auth_theater_employee
+def insert_location(*args, **kwargs):
+    data = request.get_json()
+
+    location_data = {
+        "location": data["location"],
+        "created": datetime.datetime.utcnow(),
+        "user": kwargs["user"]
+    }
+    location_id = cmpe202_db_client.locations.insert_one(location_data).inserted_id
+
+    logger.info("New Location Inserted : ID ({0})".format(str(location_id)))
+
+    return jsonify({"location_id": str(location_id)})
+
+
+@theater_employee.route('/api/theater_employee/get_locations', methods=['GET'])
+@check_auth_theater_employee
+def get_locations(*args, **kwargs):
+    response = []
+
+    locations_cursor = cmpe202_db_client.locations.find({
+        "$or": [
+            {"deleted": {"$exists": False}},
+            {"deleted": False}
+        ]
+    })
+        
+    for rec in locations_cursor:
+        clean_obj(rec)
+        response.append(rec)
+
+    return jsonify(response)
+
+
+@theater_employee.route('/api/theater_employee/delete_location/<location_id>', methods=['DELETE'])
+@check_auth_theater_employee
+def delete_location(location_id, *args, **kwargs):
+
+    cmpe202_db_client.locations.update_one(
+        {'_id': ObjectId(location_id)}, {"$set": {"deleted": True}})
+    
+    logger.info("Location Deleted : ID ({0})".format(str(location_id)))
+
+    return jsonify({"message": "Location Deletion Successfull"})
