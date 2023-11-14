6153dc609a3bd98da1081c66bb36caadb6f3c539
Dimitar Dimitrov <mitkodimitrov50@gmail.com>
2023-11-14 11:52:38 -0800

    retrieved data and updated recent movies section

--- Frontend/src/Views/AccountInfo/AccountInfo.jsx
+++ Frontend/src/Views/AccountInfo/AccountInfo.jsx
@@ -2,51 +2,67 @@ import "./AccountInfo.scss";
 import React, {useState, useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 import { host } from "../../env";
+import { useSelector } from 'react-redux';
 
 
 const AccountInfo = () => {
+    //const userInfo = useSelector(state => state);
     const [accountInfo, setAccountInfo] = useState('');
 
     const [movies, setMovies] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
       
-    useEffect(() => {
+    const getRecentMovies = () => {
         const fetchData = async () => {
-        try {
-           // const token = localStorage.getItem('x-access-token');
-            const response = await fetch(`${host}/api/recent_movies`, {
+            fetch(`${host}/api/recent_movies`, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
-                    'x-access-token': localStorage.getItem('x-access-token'),
+                    'x-access-token': localStorage.getItem('x-access-token')
                 },
-            });
-    
-            if (!response.ok) {
-                throw new Error('Failed to fetch data');
-            }
-    
-            const data = await response.json();
-            setMovies(data);
-            setIsLoading(false);
-        } 
-        catch (error) {
-            setError('Error fetching data');
-            setIsLoading(false);
-        }
-        };
+            })
+            .then((resp) => resp.json())
+            .then((data) => {        
+                const formattedMovies = data.map((entry) => ({
+                    movieName: entry.movie.movie_name,
+                    user: entry.movie.user,
+                    createdDate: entry.movie.created,
+                    showDate: entry.show_date,
+                    theaterID: entry.theater_id,
+                }));
+                setMovies(formattedMovies);
+                setIsLoading(false);
+            } ,[]
+        )}
+
         fetchData();
+    };
+
+    useEffect(() => {
+        getRecentMovies();
     }, []);
 
+    function displayMovies() {
+        if(isLoading) {
+            return <div> Loading...</div>;
+        }
+        else {
+            return <div>
+                    <h1>Movie List</h1>
+                    {movies.map((movie, index) => (
+                        <div key={index}>
+                            <p>Movie {index + 1}</p>
+                            <p>Movie Name: {movie.movieName}</p>
+                            <p>Show Date: {movie.showDate}</p>
+                            <p>Theater ID: {movie.theaterID}</p>
+                            <hr/>
+                        </div>
+                    ))}
+                </div>
+        }
+    }
 
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
-    
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
     /* required: back end API
         API 1: get_user_info (retrieve username/membership/points value)
         API 2: get_previous_tickets (retrieve list of previous movie tickets)
@@ -75,13 +91,12 @@ const AccountInfo = () => {
                     <h1 className="title">Upcoming Movie Tickets</h1>
                     <div className="list-box">
                         <p1> list container</p1>
-
                     </div>
                 </div>
                 <div className="info-container">
                     <h1 className="title">Movies Watched (past 30 Days)</h1>
                     <div className="list-box">
-                        <p1>${movies}</p1>
+                        {displayMovies()}
                     </div>
                 </div>
             </div>
