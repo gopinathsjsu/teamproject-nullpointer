4c916e02fd227a50fe650dc0436dcc6aae7adead
Connor Pietrasik <connorgpietrasik@gmail.com>
2023-11-16 17:34:20 -0800

    added update discount

--- Backend/controllers/theater_employee.py
+++ Backend/controllers/theater_employee.py
@@ -355,33 +355,43 @@ def get_discounts(*args, **kwargs):
     return jsonify(discounts)
 
 
-#Expects in body: "name" (str) (opt) or "seating_capacity" (int) (opt)
-# @theater_employee.route('/api/theater_employee/update_theater/<theater_id>', methods=['PATCH'])
-# @check_auth(roles=["Admin"])
-# def update_theater(theater_id, *args, **kwargs):
-#     data = request.get_json()
+#Adds a new discount
+#Expects in body: "percentage" (int) (0-100, opt), "day" (int) (0-6, opt), "start_hour" (int) (0-24, opt), "end_hour" (int) (0-24, opt), 
+# "start_date" (str) (ISO 8601 datetime format, opt), "end_date" (str) (ISO 8601 datetime format, opt)
+#Can do discount by day, time, or combined. No start date means starts immediately, no end date means end in 365 days
+@theater_employee.route('/api/theater_employee/update_discount/<discount_id>', methods=['PATCH'])
+@check_auth(roles=["Admin"])
+def update_discount(discount_id, *args, **kwargs):
+    data = request.get_json()
 
-    
+    discount_data = {}
 
-#     if "name" in data:
-#         cmpe202_db_client.theaters.update_one(
-#             {"_id": ObjectId(theater_id)},
-#             {"$set": {
-#                 "name": data["name"]
-#             }}
-#         )
+    if "percentage" in data:
+        discount_data["percentage"] = data["percentage"]
 
-#     if "seating_capacity" in data:
-#         cmpe202_db_client.theaters.update_one(
-#             {"_id": ObjectId(theater_id)},
-#             {"$set": {
-#                 "seating_capacity": data["seating_capacity"]
-#             }}
-#         )
+    if "day" in data:
+        discount_data["day"] = data["day"]
 
-#     logger.info("theater : ID ({0})".format(theater_id))
+    if "start_hour" in data:
+        discount_data["start_hour"] = data["start_hour"]
 
-#     return jsonify({"message": "theater Update Successfull"})
+    if "end_hour" in data:
+        discount_data["end_hour"] = data["end_hour"]
+
+    if "start_date" in data:
+        discount_data["start_date"] = jsdate_to_datetime(data["start_date"])
+
+    if "end_date" in data:
+        discount_data["end_date"] = jsdate_to_datetime(data["end_date"])
+
+    cmpe202_db_client.discounts.update_one(
+            {"_id": ObjectId(discount_id)},
+            {"$set": discount_data}
+        )
+
+    logger.info("discount : ID ({0})".format(discount_id))
+
+    return jsonify({"message": "discount Update Successfull"})
 
 
 #Soft deletes a given discount
