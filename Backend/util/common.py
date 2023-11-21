17af803ffb630d91d731bb76181a8553dee47d7d
Rajath N R <rajath.nr117@gmail.com>
2023-11-20 19:03:23 -0800

    Modified get_active_price API in common.py

--- Backend/util/common.py
+++ Backend/util/common.py
@@ -1,3 +1,5 @@
+from bson import ObjectId
+
 import config.app_config as app_config
 from util.db_initializer import DBServiceInitializer
 from util.app_logger import AppLogger
@@ -10,6 +12,64 @@ logger = AppLogger.getInstance(__name__).getLogger()
 
 # Calculates the price of a showtime based on active discounts
 def get_active_price(showtime_id):
+
+    price_cursor = cmpe202_db_client.showtimes.aggregate(
+        [
+            {"$match": {
+                "_id": ObjectId(showtime_id),
+                "$and": [
+                    {"$or": [
+                        {"deleted": {"$exists": False}},
+                        {"deleted": False}
+                    ]}
+                ]
+            }},
+            {"$lookup": {
+                "from": "discounts",
+                "localField": "_id",
+                "foreignField": "showtime_id",
+                "as": "discount_data",
+            }},
+            {"$unwind": {
+                "path": "$discount_data", "preserveNullAndEmptyArrays": True}
+            },
+            {"$addFields": {
+              "price": {
+                "$cond": {
+                  "if": {"$eq": [{"$ifNull": ["$price", None]}, None]},
+                  "then": int(20),
+                  "else": "$price"
+                }
+              },
+              "discount": {
+                "$cond": {
+                  "if": {"$eq": [{"$ifNull": ["$discount_data", None]}, None]},
+                  "then": int(0),
+                  "else": "$discount_data.percentage"
+                }
+              }
+            }},
+            {"$project": {
+                "price": {
+                "$subtract": [
+                    "$price",
+                    {"$divide": [
+                        {"$multiply": ["$price", "$discount"]},
+                        int(100)
+                    ]}
+                ]
+              }
+            }}
+        ]
+    )
+
+    price = 20.0
+    for rec in price_cursor:
+        price = rec["price"] if "price" in rec else price
+
+    return price
+
+    '''
     rec = cmpe202_db_client.showtimes.find_one({
             "_id": showtime_id,
             "$or": [
@@ -66,4 +126,4 @@ def get_active_price(showtime_id):
     price = 20.0
 
     return price - (price * discount / 100) if discount else price
-    
+    '''
